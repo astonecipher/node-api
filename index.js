e795238e8a92d11321bbe921378e712e840ee499
Andrew.Stonecipher <andrew.stonecipher@hotmail.com>
2022-12-21 16:52:36 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+
+  app.get('/users', db.getUsers);
+  app.get('/users/:id', db.getUserById)
+  app.post('/users', db.createUser)
+  app.put('/users/:id', db.updateUser)
+  app.delete('/users/:id', db.deleteUser)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`)); 
+ 
